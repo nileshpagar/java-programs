ac97577781be16fd66af9f6b9a8bfd5ca4fa1d3b
nileshpagar <nileshpagar@gmail.com>
2017-01-25 01:16:32 +0800

    added a program to reverse integer

---
+++ src/main/java/java2novice/ReverseNumber.java
@@ -0,0 +1,29 @@
+package java2novice;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+/**
+ * Created by nileshpagar on 1/25/17.
+ */
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
+        System.out.print("input an integer to reverse : ");
+        Integer number = scanner.nextInt();
+        scanner.close();
+        System.out.printf("Reversed number is %d", reverseInteger(number));
+    }
+
+    private static Integer reverseInteger(Integer number) {
+        Integer reversedNumber = 0;
+        while (number > 0){
+            number = reversedNumber * 10 + number % 10;
+            number /= 10;
+        }
+        return reversedNumber;
+    }
+
+}
