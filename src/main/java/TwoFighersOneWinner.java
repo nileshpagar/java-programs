427040ba193ca9b6d4bbbb1cc70798814b5acdd0
Nilesh Pagar <nileshpagar@gmail.com>
2017-02-06 11:57:15 +0800

    added codewars program to determine winner from the two fighters

---
+++ src/main/java/TwoFighersOneWinner.java
@@ -0,0 +1,18 @@
+
+import static java.math.BigDecimal.valueOf;
+import static java.math.RoundingMode.CEILING;
+
+public class TwoFighersOneWinner {
+
+    public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+        int attemptsForFighter1 = valueOf(fighter2.health).divide(valueOf(fighter1.damagePerAttack), CEILING).intValue();
+        int attemptsForFighter2 = valueOf(fighter1.health).divide(valueOf(fighter2.damagePerAttack), CEILING).intValue();
+        if (attemptsForFighter1 < attemptsForFighter2 || (attemptsForFighter1 == attemptsForFighter2 && firstAttacker.equals(fighter1.name)))
+            return fighter1.name;
+        else if (attemptsForFighter2 < attemptsForFighter1 || (attemptsForFighter1 == attemptsForFighter2 && firstAttacker.equals(fighter2.name))){
+            return fighter2.name;
+        }
+        return null;
+    }
+
+}
