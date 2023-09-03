0883d26730ed2ff646cb05494fc584dfd2c896d6
nileshpagar <nileshpagar@gmail.com>
2023-09-03 15:55:09 +0800

    added code to convert int to binary

--- src/main/java/leetcode/countibits_338/CountingBits_338.java
+++ src/main/java/leetcode/countibits_338/CountingBits_338.java
@@ -8,7 +8,45 @@ import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.joining;
 
 public class CountingBits_338 {
-    public int[] countBits(int n) {
+
+
+        public int[] countBits(int n) {
+            int[] ans = new int[n + 1];
+            for (int j = 0; j <= n; j++) {
+                if (j == 0) { ans[0] = 0; continue;}
+                if (j == 1) { ans[1] = 1; continue;}
+                int count = 0;
+                int temp = j;
+                int size = (int)(Math.log(temp)/Math.log(2)) + 1;
+                for (int i = size-1; i >= 0; i--) {
+                    if(temp >= Math.pow(2, i)) {
+                        count++;
+                        temp = (int) (temp - Math.pow(2, i));
+                    }
+                }
+                ans[j] = count;
+            }
+            return ans;
+        }
+
+
+//    public int[] countBits(int n) {
+//        int[] ans = new int[n + 1];
+//        for (int i = 0; i <= n; i++) {
+//            ans[i] = countOnes(getBinary(i));
+//        }
+//        return ans;
+//    }
+
+    private int countOnes(int[] binary) {
+        int count = 0;
+        for (int i = 0; i < binary.length; i++) {
+            if (binary[i] == 1) count++;
+        }
+        return count;
+    }
+
+    private static int[] getBinary(int n) {
         int temp = n;
         if (temp <= 0 ) return new int[]{0};
         if (temp == 1 ) return new int[]{1};
@@ -43,7 +81,7 @@ public class CountingBits_338 {
 
     public static void main(String[] args) {
         CountingBits_338 countingBits338 = new CountingBits_338();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 5; i < 100; i++) {
             countingBits338.countBits(i);
         }
     }
